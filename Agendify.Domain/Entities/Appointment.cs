5a7477f60b03a65ac8535d297eda5f201dd51f3f
Lucas <Lucas015x@gmail.com>
2025-05-08 20:21:36 -0300

    feat: definindo as entidades e criando uma exceção de regra de negócio

--- Agendify.Domain/Entities/Appointment.cs
+++ Agendify.Domain/Entities/Appointment.cs
@@ -1,14 +1,39 @@
 using Agendify.Domain.Enums;
+using Agendify.Domain.Exceptions;
 
 namespace Agendify.Domain.Entities;
 
 public class Appointment
 {
-    public int Id { get; set; }
+    public Guid Id { get; set; }
     public Client Client { get; set; } =  new Client();
-    public int ClientId { get; set; }
+    public Guid ClientId { get; set; }
     public Business Business { get; set; }  = new Business();
-    public int BusinessId { get; set; }
-    public DateTime DateTime{ get; set; }
+    public Guid BusinessId { get; set; }
+    public DateTime StartTime{ get; set; }
+    public DateTime EndTime{ get; set; }
     public AppointmentStatus Status { get; set; }
+    
+    private Appointment() { }
+
+    private Appointment(Guid clientId, Guid businessId, DateTime starTime, DateTime endTime)
+    {
+        Id =  Guid.NewGuid();
+        ClientId = clientId;
+        BusinessId = businessId;
+        StartTime = starTime;
+        EndTime = endTime;
+        Status = AppointmentStatus.Pending;
+    }
+
+    public static Appointment Create(Guid clientId, Guid businessId, DateTime startTime, DateTime endTime)
+    {
+        if (startTime < DateTime.UtcNow)
+            throw new DomainException("O agendamento não pode ser feito no passado.");
+
+        if (endTime <= startTime)
+            throw new DomainException("O horário de término do compromisso deve ser posterior ao horário de início.");
+        
+        return new Appointment(clientId, businessId, startTime, endTime);
+    }
 }
--- Agendify.Domain/Entities/Client.cs
+++ Agendify.Domain/Entities/Client.cs
@@ -2,7 +2,7 @@ namespace Agendify.Domain.Entities;
 
 public class Client
 {
-    public int Id { get; set; }
+    public Guid Id { get; set; }
     public string FullName { get; set; } = String.Empty;
     public string Email { get; set; } = String.Empty;
     public string Phone { get; set; } = String.Empty;
